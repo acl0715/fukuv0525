e6e35f50bd58ecd2d4eabecbba625c9132b0b9d6
ebina <aclzoppi.0715@gmail.com>
2023-05-25 13:55:06 +0900

    復習問題

--- fuku0528/Form1.Designer.cs
+++ fuku0528/Form1.Designer.cs
@@ -28,12 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(628, 231);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 68);
+            button1.TabIndex = 0;
+            button1.Text = "↑";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(549, 305);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 68);
+            button2.TabIndex = 1;
+            button2.Text = "←";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(628, 379);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 68);
+            button3.TabIndex = 2;
+            button3.Text = "↓";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(713, 305);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 68);
+            button4.TabIndex = 3;
+            button4.Text = "→";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(187, 128);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 15);
+            label1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.Location = new Point(277, 162);
+            label2.Name = "label2";
+            label2.Size = new Size(98, 45);
+            label2.TabIndex = 0;
+            label2.Text = "label1";
+            label2.Click += label2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label label1;
+        private Label label2;
     }
 }
--- fuku0528/Form1.cs
+++ fuku0528/Form1.cs
@@ -6,5 +6,32 @@ namespace fuku0528
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Top = label2.Top - 15;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ebina");
+            label2.Visible = false;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Left = label2.Left - 15;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label2.Top = label2.Top + 15;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label2.Left = label2.Left + 15;
+        }
     }
 }
